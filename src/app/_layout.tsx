b9798a60a0484cb84a74f0fa2eb53731170b8e39
Anupa CJ <anupachiranj@gmail.com>
2026-08-22 15:11:06 +0530

    Force inject font-face for web icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,8 +4,16 @@ import { useFonts } from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
+import { Platform } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
+
+const fontStyles = `
+  @font-face {
+    font-family: 'Ionicons';
+    src: url(${require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf')}) format('truetype');
+  }
+`;
 
 export default function Layout() {
   const [loaded, error] = useFonts({
@@ -24,6 +32,7 @@ export default function Layout() {
 
   return (
     <AuthProvider>
+      {Platform.OS === 'web' && <style type="text/css">{fontStyles}</style>}
       <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#313338' } }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="chat" options={{ headerShown: false }} />
